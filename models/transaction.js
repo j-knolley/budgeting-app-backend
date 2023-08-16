316993f245ef86020cf9fa3a11047f499aeb2152
Justin Knolley <justinknolley@pursuit.org>
2023-08-16 19:06:07 -0400

    Initial commit

---
+++ models/transaction.js
@@ -0,0 +1,68 @@
+module.exports = [
+{
+    date: "April 1",
+    name: "Income",
+    amount: 4000,
+    from: "job",
+  },
+{
+    date: "April 1",
+    name: "Taxes",
+    amount: -500,
+    from: "government",
+  },
+{
+    date: "April 1",
+    name: "Rent",
+    amount: -2100,
+    from: "landlord",
+  },
+{
+    date: "April 1",
+    name: "Retirement",
+    amount: -100,
+    from: "retirement fund",
+  },
+{
+    date: "April 1",
+    name: "Savings",
+    amount: -100,
+    from: "savings account",
+  },
+{
+    date: "April 1",
+    name: "Credit card payment",
+    amount: -100,
+    from: "credit card company",
+  },
+{
+    date: "April 5",
+    name: "Birthday money from Aunt Tilda",
+    amount: 50,
+    from: "aunt tilda",
+  },
+{
+    date: "April 5",
+    name: "Coffee",
+    amount: -5,
+    from: "coffee shop",
+  },
+{
+    date: "April 5",
+    name: "Internet",
+    amount: -100,
+    from: "internet company",
+  },
+{
+    date: "April 3",
+    name: "Groceries",
+    amount: -76,
+    from: "grocery store",
+  },
+{
+    date: "April 3",
+    name: "Pet food",
+    amount: -7,
+    from: "pet storegit",
+  },
+]
